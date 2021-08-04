a3a0f481c59cabeed51488e7ae1f1fe179686a5e
Noxine <jerome.nox@gmail.com>
2021-08-04 11:55:47 +0200

    ajout d'une install .sh

---
+++ install_jenkins.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+    apt update && apt upgrade
+#Instalation de java
+    apt install default-jre -y
+
+    apt install default-jdk -y
+#instal les tools ifconfig
+	apt install net-tools -y
+
+#installation des Paquets debian jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+
+ sh -c ‘echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list’
+
+ apt-get update
+
+ apt-get install jenkins -y
+
+ cat ./var/lib/jenkins/secrets/initialAdminPassword 
+
+ #affiche l'adresse IP
+	echo""
+	echo "Voici l'adresse pour se connecter sur le navigateur (IPv4)"
+	/sbin/ifconfig ens33 | grep inet | awk '{ print $2 }'
+	echo ""
