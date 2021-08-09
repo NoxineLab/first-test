f3266a997e80a9660986e7bf9c379c3c2145a4c0
NoxineLab <88375502+NoxineLab@users.noreply.github.com>
2021-08-09 15:15:55 +0200

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -12,7 +12,7 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 
  sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
- apt-get update
+ apt update
 
  apt-get install jenkins -y
 
