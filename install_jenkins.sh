fe3c9b552ab0fcf97b5c3317512c6ecfb69dd647
NoxineLab <88375502+NoxineLab@users.noreply.github.com>
2021-08-09 15:43:19 +0200

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -10,11 +10,13 @@
 #installation des Paquets debian jenkins
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 
- sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+ sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
  apt update
 
  apt-get install jenkins -y
+ 
+ sudo systemctl enable --now jenkins
 
  cat /var/lib/jenkins/secrets/initialAdminPassword 
 
